9b46779698170ebf01bed790486b6737d3bdda55
EvzenNemna <91117474+EvzenNemna@users.noreply.github.com>
2022-01-09 17:37:37 +0100

    verze 3

--- script.js
+++ script.js
@@ -33,6 +33,15 @@ $(document).ready(function() {
   });
 });
 
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 var center = SMap.Coords.fromWGS84(14.41790, 50.12655);
 var m = new SMap(JAK.gel("m"), center, 13);
 m.addDefaultLayer(SMap.DEF_BASE).enable();
