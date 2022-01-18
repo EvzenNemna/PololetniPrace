0d9cebc0d0a36ab81a209158259e20f5ee4ce785
EvzenNemna <91117474+EvzenNemna@users.noreply.github.com>
2022-01-18 19:13:07 +0100

    Verze 5

--- script.js
+++ script.js
@@ -12,25 +12,6 @@ $(document).ready(function() {
       autplayTimeout: 4,
     });
   });
-
-  var open = false;
-
-  $("#topnav-bars").click(function() {
-    if (open == false) {
-      $("li:not(:first-child)").css("display", "initial").css("width", "100%");
-      open = true;
-    } else {
-      $("li:not(:first-child)").css("display", "none");
-      $("#topnav-bars").css("display", "initial");
-      open = false;
-    }
-  });
-
-  lightbox.option({
-    'fadeDuration': 500,
-    'imageFadeDuration': 500,
-    'resizeDuration': 400
-  });
 });
 
 function myFunction() {
@@ -42,8 +23,5 @@ function myFunction() {
   }
 }
 
-var center = SMap.Coords.fromWGS84(14.41790, 50.12655);
-var m = new SMap(JAK.gel("m"), center, 13);
-m.addDefaultLayer(SMap.DEF_BASE).enable();
 
 
